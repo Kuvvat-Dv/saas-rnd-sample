d89154774276ed71ee6ec7cea0a7f5778280941b
Kuvvat-Dv <bayramooglykuvvat@gmail.com>
2026-05-13 17:49:55 +0500

    updated

--- src/visits/views.py
+++ src/visits/views.py
@@ -3,8 +3,11 @@ from .models import PageVisit
 # Create your views here.
 
 def homePage(request):
+    username = request.user.username
+    user = username.capitalize()
     queryset = PageVisit.objects.all()
     home_html = 'home.html'
-    context = {"queryset":queryset.count(),
+    context = {"querysets":queryset,
+               "username":user,
                "list":"Hello Wen"}
     return render(request, home_html, context)
